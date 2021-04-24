1106bd4817df0dc7d629c243d523faa3c8b9ec31
Karimzhon Masaitov <kmasaitov1988@gmail.com>
2021-04-24 15:58:21 -0500

    Initial commit

--- src/day1_github/HelloWorld.java
+++ src/day1_github/HelloWorld.java
@@ -3,5 +3,6 @@ package day1_github;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("asdfghjkllloiuytrew");
     }
 }
